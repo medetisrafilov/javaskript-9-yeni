261ca0bcf486150d299842d09ba6cec22bf50408
medetisrafilov <israfilovmedet52@gmail.com>
2024-12-02 18:35:55 -1200

    Homework

---
+++ abb.js
@@ -0,0 +1,14 @@
+let text = promt("Reqem mətin Daxil Edin Daxil edin")
+let a = false 
+for(i=0; i<text.length; i++){
+    let b = text[i]
+    if(b.match(/[0-9]/)){
+        a = true
+        break}
+
+        if(a){
+            console.log("Rəqəm var")
+        }
+        else{console.log("Rəqəm Yoxdur")}
+    }
+
